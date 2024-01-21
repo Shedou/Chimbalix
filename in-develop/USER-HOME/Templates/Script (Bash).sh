c3e342967fb5703bb707bde462ca40fe057444e7
Chimbal <chimbal.chimbal@yandex.ru>
2024-01-21 16:12:16 +0400

    Update Script Template

--- in-develop/USER-HOME/Templates/Script (Bash).sh
+++ in-develop/USER-HOME/Templates/Script (Bash).sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 # Script version 1.0
-# Example Vars:
+
+# -= Examples =-
 # path_to_script="$( dirname "$(readlink -f "$0")")"
-# Add all arguments to variable: Files="$@"
-# Remove the first argument from the queue: shift
 # Current User Name: $USER
 # Pause: read pause
+
 # FileNameWithoutExtension="${FileName%.*}"
 # FileNameWithoutPath="$(basename $FileName)"
 # FilePathWithoutFileName="$(dirname $FileNameWithPath)"
+
+# Add all arguments to variable: Files="$@"
+# Remove the first argument from the queue: shift
+
+# Array arguments iterate:
+# Files=("$@")
+# for i in "${!Files[@]}"; do
+#	CurrentFile="${Files[$i]}"
+# 	Current_Array_Number="$i"
+# done
 
 # Font styles
 # Usage: "${B}Bold Text${N}"
