e64535364dc06c27fe7e379f529f5f0de2d307a8
Chimbal <chimbal.chimbal@yandex.ru>
2024-05-28 10:20:41 +0400

    Update Script (Bash).sh

--- in-develop/USER-HOME/Templates/Script (Bash).sh
+++ in-develop/USER-HOME/Templates/Script (Bash).sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 # Script version 1.0
 
-# -= Examples =-
+### -= Examples =- ###
 # Path_To_Script="$( dirname "$(readlink -f "$0")")"
 # Current User Name: $USER
 # Pause: read pause
-# Pause (press any key to exit): read -rn1
 
 # FileNameWithoutExtension="${FileName%.*}"
 # FileNameWithoutPath="$(basename $FileName)"
@@ -14,18 +13,27 @@
 #
 # sed -i -e "s~TEXT_TO_REPLACE~TEXT~g" "File.txt"
 
-# Add all arguments to variable: Files="$@"
+## Add all arguments to variable: Files="$@"
 # Remove the first argument from the queue: shift
 # get error code from last command: "$?"
 
-# Array arguments iterate:
-# Files=("$@")
-# for i in "${!Files[@]}"; do
-#	CurrentFile="${Files[$i]}"
+## Array arguments iterate:
+# Args=("$@")
+# for i in "${!Args[@]}"; do
+#	CurrentFile="${Args[$i]}"
 # 	Current_Array_Number="$i"
 # done
 
-# Font styles
+## Insert data into an array and place data from the array into another file after the line "SEARCH_THIS_TEXT_IN_FILE":
+# DirFiles=()
+# readarray -t DirFiles < <(ls "/portsoft/x86_64/")
+#
+# for Number in "${!DirFiles[@]}"; do
+#	CurrentFile="${DirFiles[$Number]}"
+#	awk -i inplace '{if($0=="SEARCH_THIS_TEXT_IN_mod_file") $0=$0"\n\"'"$CurrentFile"'\"";print}' "mod_file.sh"
+# done
+
+## Font styles
 # Usage: "${B}Bold Text${N}"
 # B=$(tput bold)
 # N=$(tput sgr0)
